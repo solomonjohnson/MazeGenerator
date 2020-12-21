eeb7d107fae32d0ba96499a2c5a6b4e0786bb601
solomon <solomonjohnson@live.in>
2020-12-21 18:13:47 +0530

    first commit

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(MazeConstructor))]
+public class GameController : MonoBehaviour
+{
+  private MazeConstructor generator;
+
+  private void Start()
+  {
+    generator = GetComponent<MazeConstructor>();
+    generator.GenerateNewMaze(13, 15);
+    //EventMa
+  }
+}
